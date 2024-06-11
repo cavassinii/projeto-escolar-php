092beb7d17aad78e8681971c3504c4b757fa0eb4
João Pedro Cavassini <joaopedrocavassini2@gmail.com>
2024-06-10 21:43:12 -0300

    Create Departamento.php

---
+++ MODEL/Departamento.php
@@ -0,0 +1,46 @@
+<?php
+namespace MODEL;
+
+class Departamento
+{
+    private ?int $id;
+    private ?string $nome;
+    private ?string $descricao;  
+
+    public function __construct()
+    {
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+
+    public function setDescricao(string $descricao)
+    {
+        $this->descricao = $descricao;
+    }
+
+}
+
+?>
