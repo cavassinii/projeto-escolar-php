da8ab9355a8718f20737913471b0da63ffb4b501
João Pedro Cavassini <joaopedrocavassini2@gmail.com>
2024-06-10 21:43:04 -0300

    Create Professor.php

---
+++ MODEL/Professor.php
@@ -0,0 +1,46 @@
+<?php
+namespace MODEL;
+
+class Professor
+{
+    private ?int $id;
+    private ?string $nome;
+    private ?string $email;
+
+    public function __construct()
+    {
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+    }
+
+}
+
+?>
